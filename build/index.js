a55857dd97f9c66fceec128bed0140293179f666
Hamidreza <hrra13801396h@gmail.com>
2021-12-31 08:26:32 +0330

    Get input from user

--- build/index.js
+++ build/index.js
@@ -1,1 +1,17 @@
 "use strict";
+const inquirer = require("inquirer");
+const commands = ["+", "*", "/", "POP", "DUP", "PSH", "END"];
+const stack = [];
+const question = {
+    name: "input",
+    message: "enter your command: ",
+};
+// get user input with inquirer
+function getInput() {
+    return inquirer.prompt([question]);
+}
+function run() {
+    getInput().then(({ input }) => {
+    });
+}
+run();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-const commands : string[] = ["+" , "*" , "/" , "POP" , "DUP" , "PSH"  , "END"];
+const inquirer = require("inquirer");
+const commands: string[] = ["+", "*", "/", "POP", "DUP", "PSH", "END"];
+const stack: string[] | number[] = [];
+type QuestionTypes = {
+  name: string;
+  message: string;
+};
+
+type ResponseInputType = {
+  input: string;
+};
+
+const question: QuestionTypes = {
+  name: "input",
+  message: "enter your command: ",
+};
+
+// get user input with inquirer
+function getInput() {
+  return inquirer.prompt([question]);
+}
+
+function run() {
+  getInput().then(({ input }: ResponseInputType) => {
+      
+  });
+}
+
+run();
