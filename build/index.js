fde1139544ce67bd1db1c834ab39bd658878945c
Hamidreza <hrra13801396h@gmail.com>
2021-12-31 09:26:52 +0330

    Create main operation functionality  , add and minus

--- build/index.js
+++ build/index.js
@@ -1,17 +1,54 @@
 "use strict";
 const inquirer = require("inquirer");
-const commands = ["+", "*", "/", "POP", "DUP", "PSH", "END"];
-const stack = [];
+const commands = ["+", "*", "/", "-", "POP", "DUP", "PSH", "END"];
+let stack = [];
 const question = {
     name: "input",
     message: "enter your command: ",
 };
+// Returns  previous numbers
+function getLastNumbers() {
+    return stack.slice(0, stack.length);
+}
 // get user input with inquirer
 function getInput() {
     return inquirer.prompt([question]);
 }
+function doOperation(operand) {
+    const numbers = getLastNumbers();
+    stack = [];
+    stack[0] = numbers.reduce((a, b) => {
+        switch (operand) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+        }
+    });
+}
+// run app
 function run() {
     getInput().then(({ input }) => {
+        if (/\d/.test(input)) {
+            stack.push(Number(input));
+        }
+        else if (!commands.includes(input)) {
+            console.log("Command not found");
+            return;
+        }
+        switch (input) {
+            // sum all the array numbers together and assign array with it's number
+            case "+":
+                doOperation("+");
+            case "-":
+                doOperation("-");
+        }
+        console.log(stack[stack.length - 1]);
+        run();
     });
 }
 run();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-const commands: string[] = ["+", "*", "/", "POP", "DUP", "PSH", "END"];
-const stack: string[] | number[] = [];
+const commands: string[] = ["+", "*", "/", "-", "POP", "DUP", "PSH", "END"];
+let stack: (string | number)[] = [];
 type QuestionTypes = {
   name: string;
   message: string;
@@ -15,14 +15,55 @@ const question: QuestionTypes = {
   message: "enter your command: ",
 };
 
+// Returns  previous numbers
+function getLastNumbers(): number[] {
+  return stack.slice(0, stack.length);
+}
+
 // get user input with inquirer
 function getInput() {
   return inquirer.prompt([question]);
 }
 
+function doOperation(operand: string): void {
+  const numbers: number[] = getLastNumbers();
+
+  stack = [];
+  stack[0] = numbers.reduce((a, b) => {
+    switch (operand) {
+      case "+":
+        return a + b;
+      case "-":
+        return a - b;
+      case "*":
+        return a * b;
+      case "/":
+        return a / b;
+    }
+  });
+}
+
+// run app
 function run() {
   getInput().then(({ input }: ResponseInputType) => {
-      
+    if (/\d/.test(input)) {
+      stack.push(Number(input));
+    } else if (!commands.includes(input)) {
+      console.log("Command not found");
+      return;
+    }
+
+    switch (input) {
+      // sum all the array numbers together and assign array with it's number
+      case "+":
+        doOperation("+");
+      case "-":
+        doOperation("-");
+    }
+
+    console.log(stack[stack.length - 1]);
+
+    run();
   });
 }
 
